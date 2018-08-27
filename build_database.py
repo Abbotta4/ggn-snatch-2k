91263ae442e785b017cc8047bac1b877eb5adada
Drew Abbott <abbotta4@gmail.com>
2018-08-27 16:22:01 -0400

    Fix import logic and added __name__ check
    Put all running code in main() and add a check for __name__ to allow for
    proper importing from build_database into snatch.py

--- build_database.py
+++ build_database.py
@@ -2,19 +2,8 @@
 
 import requests, json, configparser, sqlite3, time
 
-# Load the configuration file
-try:
-    with open("config.ini") as f:
-        config = configparser.ConfigParser()
-        config.read('config.ini')
-except:
-    print('Could not find a config file.')
-    exit()
-
-BASEURL = config['DEFAULT']['baseurl']
-API_KEY = config['DEFAULT']['api_key']
-START_ID = config['DEFAULT']['start_id']
-END_ID = config['DEFAULT']['end_id']
+if __name__ == '__main__':
+    main()
 
 class db_cursor:
     def __init__(self):
@@ -45,12 +34,28 @@ def poll_api(tries, initial_delay, delay, backoff, url):
     polling_time = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
     raise ExceededRetries("{0}. Failed to poll {1} within {2} tries.".format(polling_time, url, tries))
 
-for id in range(int(START_ID), int(END_ID)):
-    url = BASEURL + 'api.php?request=torrent&id=' + str(id) + '&key=' + API_KEY
-    j = poll_api(10, 2, 1, 2, url)
-    polling_time = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
-    print("{0}. ID {1} {2}".format(polling_time, id, '\033[92mSUCCESS\033[0m' if j['status'] == 'success' else '\033[91mFAILURE\033[0m'))
-    if j['status'] == 'success':
-        j = j['response']['torrent']
-        with db_cursor() as cursor:
-            cursor.execute('''REPLACE INTO torrents (id, name, size) VALUES (?, ?, ?)''', (id, j['releaseTitle'], j['size']))
+def main():
+    # Load the configuration file
+    try:
+        with open("config.ini") as f:
+            config = configparser.ConfigParser()
+            config.read('config.ini')
+    except:
+        print('Could not find a config file.')
+        exit()
+
+    BASEURL = config['DEFAULT']['baseurl']
+    API_KEY = config['DEFAULT']['api_key']
+    START_ID = config['DEFAULT']['start_id']
+    END_ID = config['DEFAULT']['end_id']
+
+
+    for id in range(int(START_ID), int(END_ID)):
+        url = BASEURL + 'api.php?request=torrent&id=' + str(id) + '&key=' + API_KEY
+        j = poll_api(10, 2, 1, 2, url)
+        polling_time = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
+        print("{0}. ID {1} {2}".format(polling_time, id, '\033[92mSUCCESS\033[0m' if j['status'] == 'success' else '\033[91mFAILURE\033[0m'))
+        if j['status'] == 'success':
+            j = j['response']['torrent']
+            with db_cursor() as cursor:
+                cursor.execute('''REPLACE INTO torrents (id, name, size) VALUES (?, ?, ?)''', (id, j['releaseTitle'], j['size']))
